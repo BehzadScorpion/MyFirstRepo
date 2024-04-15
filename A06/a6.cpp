6cfe3a5b488458a87cec0d45dc6b593d20c717d4
BehzadScorpion <52175983+BehzadScorpion@users.noreply.github.com>
2024-04-14 22:33:00 -0400

    second commit

---
+++ A06/a6.cpp
@@ -0,0 +1,18 @@
+/*
+* FILE : a6.cpp
+* PROJECT : PROG1350 - Assignment #6
+* PROGRAMMER : Behzad Afrasiabi
+* FIRST VERSION : 2024-04-14
+* DESCRIPTION :
+*	This simple program was made to satisfy the requirements for
+*	SEF Assignment #6		
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("first push");
+
+	return 0;
+}
